6724f58f33e6e22388949daa849ffb8cc8ade60b
LazyFoox <53003620+LazyFoox@users.noreply.github.com>
2021-09-14 16:29:49 +0800

    Update Random.py

--- Python/Random.py
+++ Python/Random.py
@@ -1,4 +1,18 @@
+# importing Pip
 import random
 from random import randint
-T1 = randint(0,255)
-print(T1)
+
+
+# random text
+
+T1 = [
+    "Hello",
+    "Testing",
+    "Haaaaaaa",
+] # getting some text in there
+print(random.choice(T1)) # randomized picking from the get text
+
+# Random integers
+
+T2 = randint(0,255) # Getting a randomize number
+print(T2) #printing the finished random number
