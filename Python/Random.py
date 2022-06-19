62b0bf4ce6bc8a7d36c3924f213527ad4a01f776
LazyFoox <53003620+LazyFoox@users.noreply.github.com>
2022-06-19 23:21:48 +0800

    Update Random.py

--- Python/Random.py
+++ Python/Random.py
@@ -16,3 +16,16 @@ print(random.choice(T1)) # randomized picking from the get text
 
 T2 = randint(0,255) # Getting a randomize number
 print(T2) #printing the finished random number
+
+# password
+
+text = "abcdefghijklmnopqrstuvwxyz"
+TEXT = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num = "1234567890"
+special = "@#$&-+_\/*"
+
+mix = text + TEXT + num + special
+length = 8
+
+final = "".join(random.sample(mix, length)) #random.sample is a scrambled random words
+print(final)
